c77218b9a84ff662722374794c4b298462801b8f
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-03-01 00:03:41 +0530

    A better way to Organize Code

---
+++ src/state/actions/index.ts
@@ -0,0 +1,19 @@
+import { ActionType } from "../action-types";
+interface SearchRepositories {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesError {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+export type Action =
+  | SearchRepositories
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesError;
--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,31 +1,9 @@
+import { Action } from "../actions";
+import { ActionType } from "../action-types";
 interface RepositoriesState {
   loading: boolean;
   error: string | null;
   data: string[];
-}
-interface SearchRepositories {
-  type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-  payload: string[];
-}
-
-interface SearchRepositoriesError {
-  type: ActionType.SEARCH_REPOSITORIES_ERROR;
-  payload: string;
-}
-
-type Action =
-  | SearchRepositories
-  | SearchRepositoriesSuccessAction
-  | SearchRepositoriesError;
-
-enum ActionType {
-  SEARCH_REPOSITORIES = "search_repositories",
-  SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
-  SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
 }
 
 const reducer = (

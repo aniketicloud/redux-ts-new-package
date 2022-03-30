e32d7a1f41d397ca8da1ed1e9859d53ea84d284f
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-03-30 19:30:48 +0530

    Type/interface for Action

--- src/store/reducers/repositoriesReducer.ts
+++ src/store/reducers/repositoriesReducer.ts
@@ -4,13 +4,21 @@ interface RepositoriesState {
   data: string[];
 }
 
+interface Action {
+  type: string;
+  payload?: any;
+}
+
 enum ActionTypes {
   SEARCH_REPOSITORIES = "search_repositories",
   SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
   SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
 }
 
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
   switch (action.type) {
     case ActionTypes.SEARCH_REPOSITORIES:
       return { loading: false, error: null, data: [] };

c98f45d062ccf111349d531277f83c947588cc77
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-03 21:24:13 +0530

    adding request logic

--- src/store/action-creators/index.ts
+++ src/store/action-creators/index.ts
@@ -3,13 +3,29 @@ import { ActionType } from "../action-types";
 // import { ActionType } from "../action-types";
 // import { Action } from "../actions";
 
-const search_repositories = (searchTerm) => {
+const npm_search_url: string = "https://registry.npmjs.org/-/v1/search";
+
+export const search_repositories = (searchTerm: string) => {
   return async (dispatch: any) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
     });
 
     try {
+      const { data } = await axios.get(npm_search_url, {
+        params: {
+          text: searchTerm,
+        },
+      });
+
+      const package_names = data.objects.map(
+        (object: { package: { name: string } }) => object.package.name
+      );
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: package_names,
+      });
     } catch (err) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
